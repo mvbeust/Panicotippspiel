a31ec8a6658ad850eca9b928d0863d85c72cbf5a
Max v. Beust <maxvonbeust@gmail.com>
2015-10-07 12:14:14 +0200

    Max Logo Footer

--- footer.inc.php
+++ footer.inc.php
@@ -18,22 +18,22 @@
     </div>
   </div>
   <div class="footer-copyright" style="background:#3f51b5; border-top:#CCC; border-top:thin; border-top-style:solid">
-    <div class="container"> 
+    <div class="container">
     &#169; 2015 Panicotippspiel
     <div class="right">
     <a href="/feed">
-    <img src="/img/rss.png" height="12px" style="margin-right:10px"; />   
+    <img src="/img/rss.png" height="12px" style="margin-right:10px"; />
     </a>
     <a href="http://twitter.com/panicotippspiel" target="_blank">
-    <img src="/img/twitter.png" height="12px" style="margin-right:10px"; />   
+    <img src="/img/twitter.png" height="12px" style="margin-right:10px"; />
     </a>
     <a href="http://materializecss.com" target="_blank">
-    <img src="/img/materializecss.png" height="12px" />
+    <img src="/img/materializecss.png" height="12px" style="margin-right:10px" />
     </a>
     <a href="http://maxvonbeust.com" target="_blank">
-    <img src="/img/maxvonbeust.png" height="15px" />
+    <img src="/img/maxvonbeust.png" height="12px" />
     </a>
-    </div>	
+    </div>
     </div>
   </div>
   <!-- Google Analytics-->
