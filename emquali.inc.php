96a24774be3c8062e764adae76dadf675f3c855e
Max v. Beust <maxvonbeust@gmail.com>
2015-11-11 11:49:19 +0100

    EM Quali ist vorbei

--- emquali.inc.php
+++ emquali.inc.php
@@ -1,14 +1,14 @@
 <?php
-	 	
+
       $cachefile = "cache/euroquali.html";
-      $cachetime = 1500 * 60; // 5 minutes
-	  
+      $cachetime = 150000000 * 60; // 5 minutes
+
       // Serve from the cache if it is younger than $cachetime
       if (file_exists($cachefile) && (time() - $cachetime
-         < filemtime($cachefile))) 
+         < filemtime($cachefile)))
       {
          include($cachefile);
-         echo "<!-- Cached ".date('jS F Y H:i', filemtime($cachefile))." 
+         echo "<!-- Cached ".date('jS F Y H:i', filemtime($cachefile))."
          -->";
          exit;
       }
@@ -16,7 +16,7 @@
 ?>
     	<?php
 
-	  // Parsing this spreadsheet: 
+	  // Parsing this spreadsheet:
 	  $url = 'https://spreadsheets.google.com/feeds/list/1DfqbxCNBjXuTiCujaSa-Ze4xYDA5VpHQ1LSvFmPFuD0/oxvr4x6/public/values?alt=json';
 	  $file= file_get_contents($url);
 	  $i = 0;
@@ -54,7 +54,7 @@
 		   $i = 0;
 		   $c = $c +1;
 	   };
-	  };      
+	  };
 	  ?>
       </tbody></table></div></li>
   <script>
@@ -62,11 +62,11 @@
   </script>
 <?php
        // open the cache file for writing
-       $fp = fopen($cachefile, 'w'); 
+       $fp = fopen($cachefile, 'w');
        // save the contents of output buffer to the file
 	    fwrite($fp, ob_get_contents());
 		// close the file
-        fclose($fp); 
+        fclose($fp);
 		// Send the output to the browser
-        ob_end_flush(); 
+        ob_end_flush();
 ?>
